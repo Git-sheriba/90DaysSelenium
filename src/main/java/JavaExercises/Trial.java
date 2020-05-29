c69b2dda76d3e0f4e80884c69b2f37cc177db212
Jesus <Jesus@SHERIBA>
2020-05-30 00:48:10 +0530

    Trial modified

--- src/main/java/JavaExercises/Trial.java
+++ src/main/java/JavaExercises/Trial.java
@@ -1,31 +1,32 @@
 package JavaExercises;
-// aadded a comment for testing the git
-// This just prints the reversed string. But is not stored anywhere in a variable to be used later.
 public class Trial {
-	public static void main(String[] args) {
-		String inputString = "When the world realise its own mistake, corona will dissolve automatically";
-		String[] splitStr = inputString.split(" ");
-		String[] revStr = new String[splitStr.length]; // Empty array of same size as splitStr array
-		
-		for (int i = 0; i < splitStr.length; i++) {
-			String rev="";
-			if ((i%2)!=0) {    // check for the odd index and reverse the particular string
-				int lengthSubStr = splitStr[i].length();
-				for (int j = lengthSubStr-1; j >=0; j--) {
-					System.out.print(splitStr[i].charAt(j));
-				}
-			}
-			else {
-				System.out.print(splitStr[i]);
-			}
-			System.out.print(" ");
-		}
-		System.out.println(" ");
-		System.out.println("Input String : "+inputString);
-		
-	}
-}
-/*Write a java program to find the count the characters in the given string
-String str = "Karma is powerful than God"
-{K=1, a=3, r=2, m=1,  =4, i=1, s=1, p=1, o=2, w=1, e=1, f=1, u=1, l=1, t=1, h=1, n=1, G=1, d=1}*/
 
+    public static void main(String[] args) {
+
+        int low = 1, high = 99999;
+
+        for(int number = low + 1; number < high; ++number) {
+            int digits = 0;
+            int result = 0;
+            int originalNumber = number;
+
+            // number of digits calculation
+            while (originalNumber != 0) {
+                originalNumber /= 10;
+                ++digits;
+            }
+
+            originalNumber = number;
+
+            // result contains sum of nth power of its digits
+            while (originalNumber != 0) {
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digits);
+                originalNumber /= 10;
+            }
+
+            if (result == number)
+                System.out.print(number + " ");
+        }
+    }
+}
